5d79a7c426be5dbbf612ec3d69f9c3e96bc24bd7
melo <melo.lin@ufispace.com>
2023-12-04 17:41:38 +0800

    the date is wrong

--- platformi/Mfan/M_fan_test.h
+++ platformi/Mfan/M_fan_test.h
@@ -23,10 +23,10 @@ void t_printf(const char* format, ...) {
 
     // 取得當前時間
     gettimeofday(&tv, NULL);
-    tm_info = localtime(&tv.tv_usec);
+    tm_info = localtime(&tv.tv_sec);
 
     // 印出時間戳記（包括微秒）
-    printf("[%04d-%02d-%02d %02d:%02d:%02d.%06ld]\n", 
+    printf("[ %04d-%02d-%02d %02d:%02d:%02d.%06ld ]\n", 
            tm_info->tm_year + 1900, tm_info->tm_mon + 1, tm_info->tm_mday,
            tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec, tv.tv_usec);
 
