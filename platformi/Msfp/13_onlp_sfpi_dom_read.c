75b0f65ead8ac34afe9c68e4030c5f2796b5e5b3
melo <melo.lin@ufispace.com>
2024-03-22 18:05:50 +0800

    done with test case 13....

--- platformi/Msfp/13_onlp_sfpi_dom_read.c
+++ platformi/Msfp/13_onlp_sfpi_dom_read.c
@@ -1,40 +1,56 @@
-/***************************************************
-    Melo Try onlp test
-    This file should not be add in ONL prtoject
-***************************************************/
+#include <stdio.h>
+#include <stdint.h>
 #include "M_sfp_test.h"
-//#include "config.h"
+
 #define SFP_PORT_MAX_NUM    2
 #define QSFP_PORT_MAX_NUM    32
 
-
-int main()
-{
+int main() {
     uint8_t data[256] = {0};
+    int rc = -1;
     
+    onlp_init();
+    printf("Initialization done\n");
+
     /* Normal Case */
-    for(int port=QSFP_PORT_MAX_NUM+1; port<=QSFP_PORT_MAX_NUM+SFP_PORT_MAX_NUM; port++)
-    {
-        if(onlp_sfpi_dom_read(port, data) < 0)
-        {
-            printf("[FAILED] Port= %d is QSFP, QSFP not support onlp_sfpi_dom_read()\n");
-        }
-        else
-        {
-            printf("[PASSED] Port= %d is SFP, SFP shall support onlp_sfpi_dom_read()\n");
+    for (int port = 33; port < (33+SFP_PORT_MAX_NUM); port++) {
+        rc = onlp_sfpi_dom_read(port, data);
+        if (rc < 0) {                           
+            printf("[FAILED] Port= %d is QSFP, QSFP not supported by onlp_sfpi_dom_read()\n", port);
+        } else {
+            printf("[PASSED] Port= %d is SFP, SFP supported by onlp_sfpi_dom_read()\n", port);
         }
     }
 
-    /* Error Case */
-    for(int port=1; port<=QSFP_PORT_MAX_NUM; port++)
-    {
-        if(onlp_sfpi_dom_read(port, data) < 0)
-        {
-            printf("[PASSED] Port= %d is QSFP, QSFP not support onlp_sfpi_dom_read() is correct\n");
-        } 
-        else
-        {
-            printf("[FAILED] Port= %d is QSFP, QSFP shall not support onlp_sfpi_dom_read() but here seems to support\n");
+    /* Error Case - QSFP */
+    for (int port = 1; port <= QSFP_PORT_MAX_NUM; port++) {
+        rc = onlp_sfpi_dom_read(port, data);
+        if (rc < 0) {
+            printf("[PASSED] Port= %d is QSFP, QSFP not supported by onlp_sfpi_dom_read() is correct\n", port);
+        } else {
+            printf("[FAILED] Port= %d is QSFP, QSFP should not support onlp_sfpi_dom_read() but it seems to support\n", port);
         }
     }
+
+    /* Error Case - undefined port */
+    int port=-1;
+    rc = onlp_sfpi_dom_read(port, data);
+    if (rc < 0) {
+        printf("[PASSED] Port= %d is not a valid port, shall not supported by onlp_sfpi_dom_read() is correct\n", port);
+    } else {
+        printf("[FAILED] Port= %d is not a valid port, shall not support onlp_sfpi_dom_read() but it seems to support\n", port);
+    }
+
+    port=35;
+    rc = onlp_sfpi_dom_read(port, data);
+    if (rc < 0) {
+        printf("[PASSED] Port= %d is not a valid port, shall not supported by onlp_sfpi_dom_read() is correct\n", port);
+    } else {
+        printf("[FAILED] Port= %d is not a valid port, shall should not support onlp_sfpi_dom_read() but it seems to support\n", port);
+    }
+    
+
+    onlp_denit();
+    printf("De-initialization done\n");
+    return 0;
 }
--- platformi/Msfp/M_sfp_test.h
+++ platformi/Msfp/M_sfp_test.h
@@ -11,87 +11,20 @@
 #include <stdarg.h>
 #include <time.h>
 #include <sys/time.h>
+//#include <stdint.h>
 
-#define ONLP_CONFIG_INFO_STR_MAX 64
+typedef unsigned char uint8_t;
 
-/* printf with timestamp in microseconds (Melo)
-***********************************************
-     (1 second = 1,000,000 microseconds) */
-void t_printf(const char* format, ...) {
-    struct timeval tv;
-    struct tm* tm_info;
-
-    // 取得當前時間
-    gettimeofday(&tv, NULL);
-    tm_info = localtime(&tv.tv_sec);
-
-    // 印出時間戳記（包括微秒）
-    printf("[%04d/%02d/%02d %02d:%02d:%02d.%06ld]  ", 
-           tm_info->tm_year + 1900, tm_info->tm_mon + 1, tm_info->tm_mday,
-           tm_info->tm_hour, tm_info->tm_min, tm_info->tm_sec, tv.tv_usec);
-
-    // 使用可變引數列表處理訊息
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-
-    printf("\n");
-}
-/*void t_printf(const char* format, ...) {
-    // 開始測量時間
-    struct timeval start_time;
-    gettimeofday(&start_time, NULL);
-
-    // 使用可變引數列表處理訊息
-    va_list args;
-    va_start(args, format);
-    vprintf(format, args);
-    va_end(args);
-
-    // 結束測量時間
-    struct timeval end_time;
-    gettimeofday(&end_time, NULL);
-
-    // 計算所經過的時間（以微秒為單位）
-    long elapsed_time = (end_time.tv_sec - start_time.tv_sec) * 1000000 +
-                        (end_time.tv_usec - start_time.tv_usec);
-
-    // 印出執行時間
-    printf("\nExecution Time: %ld ms\n", elapsed_time);
-}*/
-
-
-//read config file
-void readConfigFile(const char *filename) {
-    FILE *file = fopen(filename, "r");
-    if (!file) {
-        perror("Error opening file");
-        exit(EXIT_FAILURE);
-    }
-
-    char line[256];
-
-    while (fgets(line, sizeof(line), file)) {
-        // Ignore comments and empty lines
-        if (line[0] == '#' || line[0] == '\n') {
-            continue;
-        }
-
-        // Parse each line and extract configuration data
-        char *key = strtok(line, "=");
-        char *value = strtok(NULL, "=");
-
-        // Remove leading and trailing whitespaces from key and value
-        key = strtok(key, " \t\n\r");
-        value = strtok(value, " \t\n\r");
-
-        // Process key and value as needed
-        printf("Key: %s, Value: %s\n", key, value);
-    }
-
-    fclose(file);
-}
+typedef enum onlp_status_e {
+    ONLP_STATUS_OK = 0,
+    ONLP_STATUS_E_GENERIC = -1,
+    ONLP_STATUS_E_UNSUPPORTED = -10,
+    ONLP_STATUS_E_MISSING = -11,
+    ONLP_STATUS_E_INVALID = -12,
+    ONLP_STATUS_E_INTERNAL = -13,
+    ONLP_STATUS_E_PARAM = -14,
+    ONLP_STATUS_E_I2C = -15,
+} onlp_status_t;
 
 
 /* Test funciton */
